ad719445d4e44acb7184f88dbe2ecfd927cdf7ef
thinhv <thinh.vo@wolt.com>
2024-11-15 10:49:06 +0200

    Initial Commit

---
+++ Movesic/MovesicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovesicApp.swift
+//  Movesic
+//
+//  Created by thinh on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MovesicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovesicTests/MovesicTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovesicTests.swift
+//  MovesicTests
+//
+//  Created by thinh on 15.11.2024.
+//
+
+import Testing
+@testable import Movesic
+
+struct MovesicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
